0dc7b2d2285b0ef7ecd62e08ca6b9b8e0d59cca4
Henry Wang <henry.h.wang93@gmail.com>
2020-06-11 14:04:58 -0400

    set InputForm state upon change in input boxes

--- client/src/InputForm.jsx
+++ client/src/InputForm.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 class AddCow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: '',
+      description: '',
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleDescriptionChange(event) {
+    this.setState({
+      description: event.target.value,
+    });
   }
 
   render() {
@@ -12,13 +29,23 @@ class AddCow extends React.Component {
         <div>
           <label>
             Input Name
-            <input type="text" placeholder="name of cow..."></input>
+            <input
+              type="text"
+              placeholder="name of cow..."
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            ></input>
           </label>
         </div>
         <div>
           <label>
             Input Description
-            <input type="text" placeholder="name of cow..."></input>
+            <input
+              type="text"
+              placeholder="description of cow..."
+              value={this.state.description}
+              onChange={this.handleDescriptionChange}
+            ></input>
           </label>
         </div>
         <input type="submit" value="Add Cow" />
